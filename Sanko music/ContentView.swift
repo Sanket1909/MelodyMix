83020b96b026eab8a4f675f04fa703866bd3ded1
Fitnessarena <fitnessarenaapp@gmail.com>
2022-04-03 16:18:22 -0400

    Initial Commit

---
+++ Sanko music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sanko music
+//
+//  Created by Sanket Patel  on 2022-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sanko music/Sanko_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sanko_musicApp.swift
+//  Sanko music
+//
+//  Created by Sanket Patel  on 2022-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Sanko_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
